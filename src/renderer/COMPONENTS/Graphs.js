9b0996b0bc5c563ca566eb43e864b0d7ed7ecd35
GiftGiwa <gdg2131@columbia.edu>
2024-02-11 21:28:58 -0500

    add some 2D graph code

--- src/renderer/COMPONENTS/Graphs.js
+++ src/renderer/COMPONENTS/Graphs.js
@@ -52,9 +52,9 @@ const GraphPaper = () => {
     const svg = svgRef.current;
 
     // Define the size and number of rows and columns
-    const gridSize = 800;
-    const numRows = 50;
-    const numCols = 50;
+    const gridSize = 1200;
+    const numRows = 40;
+    const numCols = 40;
 
     // Calculate cell size based on the grid size
     const cellSize = gridSize / numRows;
@@ -69,7 +69,7 @@ const GraphPaper = () => {
       horizontalLine.setAttribute('y1', y);
       horizontalLine.setAttribute('x2', gridSize.toString());
       horizontalLine.setAttribute('y2', y);
-      horizontalLine.setAttribute('stroke', 'black');
+      horizontalLine.setAttribute('stroke', 'rgba(0, 0, 0, 0.1)');
       horizontalLine.setAttribute('stroke-width', '1');
       svg.appendChild(horizontalLine);
     }
@@ -84,7 +84,7 @@ const GraphPaper = () => {
       verticalLine.setAttribute('y1', '0');
       verticalLine.setAttribute('x2', x);
       verticalLine.setAttribute('y2', gridSize.toString());
-      verticalLine.setAttribute('stroke', 'black');
+      verticalLine.setAttribute('stroke', 'rgba(0, 0, 0, 0.1)');
       verticalLine.setAttribute('stroke-width', '1');
       svg.appendChild(verticalLine);
     }
@@ -94,7 +94,8 @@ const GraphPaper = () => {
 
   return (
     <div id = "graph-paper">
-      <svg id = "grid" ref={svgRef} width="800" height="600" style={{ border: '1px solid black' }}></svg>
+      {/* <svg id = "grid" ref={svgRef} width="800" height="600" style={{ border: '1px solid black' }}></svg> */}
+      <svg id = "grid" ref={svgRef} width="1200" height="1200"></svg>
 
 
     </div>
@@ -113,9 +114,6 @@ export default function Graphs() {
 
   setTimeout(() => {
     console.log(document.getElementById("GRAPHS"))
-
-
-
   }, 100)
 
 
@@ -127,33 +125,6 @@ export default function Graphs() {
       </button>
 
       <GraphPaper></GraphPaper>
-
-      {/* <TransformWrapper className = "transform-box">
-      <Controls />
-        <TransformComponent>
-          <img
-            src="https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-            alt="test"
-            width="100%"
-          />
-        </TransformComponent>
-      </TransformWrapper> */}
-
-      {/* <div className="svgtest">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="48"
-        height="48"
-        fill="currentColor"
-        class="bi bi-google"
-        viewBox="0 0 16 16"
-      >
-        <path d="M15.545 6.558a9.42 9.42 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.689 7.689 0 0 1 5.352 2.082l-2.284 2.284A4.347 4.347 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.792 4.792 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.702 3.702 0 0 0 1.599-2.431H8v-3.08h7.545z" />
-      </svg>
-    </div> */}
-
-      {/* experimental (subject to change) */}
-      {/* <Canvas id = "canvas"></Canvas> */}
 
     </div>
   )
